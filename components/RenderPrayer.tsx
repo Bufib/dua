446fcc832bff8424e602d2dc8f6a64cc22dcf612
Bufib <bufib.bufib@gmail.com>
2025-03-17 06:06:36 +0100

    prayer

--- components/RenderPrayer.tsx
+++ components/RenderPrayer.tsx
@@ -1,6 +1,6 @@
 // export default RenderPrayer;
 
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import {
   getPrayerWithTranslations,
@@ -15,6 +15,7 @@ import { ThemedText } from "./ThemedText";
 import { useColorScheme } from "react-native";
 import i18n from "@/utils/i18n";
 import { Colors } from "@/constants/Colors";
+import { ScrollView } from "react-native";
 const RenderPrayer = () => {
   const { prayerId } = useLocalSearchParams();
   const [isLoading, setIsLoading] = useState(true);
@@ -35,7 +36,6 @@ const RenderPrayer = () => {
         );
         // All the Prayer with all the translations for this specific ID
         setPrayers(prayer);
-
         console.log(prayer);
       } catch (error: any) {
         console.log("RenderPrayer fetchPrayerData " + error);
@@ -47,19 +47,23 @@ const RenderPrayer = () => {
   }, [prayerId]);
 
   const formatePrayer = (data: string) => {
-    const formatedPrayer = data.split(" ");
+    const formatedPrayer = data.split("\n");
     return formatedPrayer;
   };
 
+  const fusePrayer = () => {
+
+  }
   return (
-    <ThemedView style={styles.container}>
+    <ScrollView
+      style={[
+        styles.container,
+        { backgroundColor: Colors[colorScheme].background },
+      ]}
+      contentContainerStyle={styles.scrollContent}
+    >
       <ThemedView style={styles.headerContainer}>
-        <View
-          style={[
-            styles.header,
-            { backgroundColor: Colors[colorScheme].contrast },
-          ]}
-        >
+        <ThemedView style={styles.header}>
           <View style={styles.headerNameContainer}>
             <ThemedText style={styles.headerTitleText} type="subtitle">
               {prayers && prayers.name}
@@ -69,18 +73,67 @@ const RenderPrayer = () => {
               {prayers && prayers.arabic_title}
             </ThemedText>
           </View>
-          <ThemedText style={styles.headerIntroduction} type="defaultSemiBold">
-            {prayers
-              ? prayers.translated_languages === null ||
-                !prayers.translated_languages.includes(language)
-                ? prayers.arabic_introduction
-                : prayers.translations.find((t) => t.language_code === language)
-                    ?.introduction || prayers.arabic_introduction
-              : null}
-          </ThemedText>
-        </View>
+          <View
+            style={[
+              styles.headerIntroductionContainer,
+              { backgroundColor: Colors[colorScheme].contrast },
+            ]}
+          >
+            <ThemedText
+              style={styles.headerIntroduction}
+              type="defaultSemiBold"
+            >
+              {prayers
+                ? prayers.translated_languages === null ||
+                  !prayers.translated_languages.includes(language)
+                  ? prayers.arabic_introduction
+                  : prayers.translations.find(
+                      (t) => t.language_code === language
+                    )?.introduction || prayers.arabic_introduction
+                : null}
+            </ThemedText>
+          </View>
+        </ThemedView>
       </ThemedView>
-    </ThemedView>
+      <ThemedView style={styles.languageContainer}>
+        <ScrollView
+          style={styles.languageButtonsScroll}
+          contentContainerStyle={styles.languageButtonsScrollContent}
+          horizontal
+        >
+          {prayers &&
+            prayers.translated_languages.map((language) => (
+              <TouchableOpacity
+                key={language.toString()}
+                style={styles.languageButton}
+              >
+                <Text>{language}</Text>
+              </TouchableOpacity>
+            ))}
+        </ScrollView>
+      </ThemedView>
+      <ThemedView style={styles.bodyContainer}>
+        {prayers &&
+          formatePrayer(prayers.arabic_text ?? "")
+            .filter((prayer) => prayer.trim() !== "")
+            .map((prayer, index) => (
+              <View
+                style={[
+                  styles.prayerContainer,
+                  { backgroundColor: Colors[colorScheme].contrast },
+                ]}
+              >
+                <Text
+                  style={[styles.prayerText, styles.prayerTextArabic]}
+                  key={index}
+                >
+                  {prayer}
+                </Text>
+                <Text style={styles.prayerIndex}> {index + 1}</Text>
+              </View>
+            ))}
+      </ThemedView>
+    </ScrollView>
   );
 };
 
@@ -90,15 +143,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  scrollContent: {
+    gap: 20,
+  },
   headerContainer: {
-    flex: 1 / 3,
+    flex: 1,
   },
   header: {
     flex: 1,
-    padding: 15,
     margin: 10,
-    borderWidth: 1,
-    borderRadius: 10,
     gap: 10,
   },
   headerNameContainer: {
@@ -107,7 +160,46 @@ const styles = StyleSheet.create({
     gap: 5,
   },
   headerTitleText: {},
+  headerIntroductionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
   headerIntroduction: {},
-  bodyContainer: {},
+  bodyContainer: {
+    flex: 1,
+    gap: 10,
+  },
+  prayerContainer: {
+    flex: 1,
+    padding: 10,
+    gap: 30
+  },
+  prayerText: {
+    fontSize: 28,
+    color: Colors.universal.secondary,
+  },
+  prayerIndex: {
+    alignSelf: "center"
+  },
+  prayerTextArabic: {
+    textAlign: "right",
+  },
+
+  languageContainer: {
+    flex: 1,
+  },
+  languageButtonsScroll: {},
+  languageButtonsScrollContent: {
+    marginLeft: 15,
+    gap: 10,
+  },
+  languageButton: {
+    padding: 10,
+    backgroundColor: Colors.universal.secondary,
+    borderRadius: 7,
+  },
   footerContainer: {},
 });
